4689d1c11f3dce746a7c1930390a9094a3b918d1
JuanjuanCaier <7688619@qq.com>
2024-12-04 23:18:59 +0800

    111

--- StardewValley/Classes/AppDelegate.cpp
+++ StardewValley/Classes/AppDelegate.cpp
@@ -83,9 +83,9 @@ bool AppDelegate::applicationDidFinishLaunching() {
     auto glview = director->getOpenGLView();
     if(!glview) {
 #if (CC_TARGET_PLATFORM == CC_PLATFORM_WIN32) || (CC_TARGET_PLATFORM == CC_PLATFORM_MAC) || (CC_TARGET_PLATFORM == CC_PLATFORM_LINUX)
-        glview = GLViewImpl::createWithRect("project", cocos2d::Rect(0, 0, designResolutionSize.width, designResolutionSize.height));
+        glview = GLViewImpl::createWithRect("StardewValley", cocos2d::Rect(0, 0, designResolutionSize.width, designResolutionSize.height));
 #else
-        glview = GLViewImpl::create("project");
+        glview = GLViewImpl::create("StardewValley");
 #endif
         director->setOpenGLView(glview);
     }
