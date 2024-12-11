87a40346175f305e759381b8975051d35095568b
JuanjuanCaier <7688619@qq.com>
2024-12-12 02:39:49 +0800

    封装了玩家类，可以正常移动，但仍然无法播放动画

--- StardewValley/Classes/MainMap.h
+++ StardewValley/Classes/MainMap.h
@@ -3,6 +3,7 @@
 
 #include "cocos2d.h"
 #include "BackpackManager.h"
+#include "Player.h"
 
 class MainMap : public cocos2d::Scene
 {
@@ -15,49 +16,18 @@ public:
     // 前往主菜单场景
     void toMenu(Ref*);
 
-    // 键盘按下事件处理
-    void onKeyPressed(cocos2d::EventKeyboard::KeyCode keyCode, cocos2d::Event* event);
-
-    // 键盘释放事件处理
-    void onKeyReleased(cocos2d::EventKeyboard::KeyCode keyCode, cocos2d::Event* event);
-
     // 背包按钮的回调函数
     void onBackpackButtonClicked(Ref* sender);
 
     // 实现create函数
     CREATE_FUNC(MainMap);
 
-private:
-    cocos2d::Sprite* mapSprite;
-    cocos2d::Sprite* playerSprite; // 主角精灵
-    cocos2d::Animation* walkAnimations[6]; // 六个方向的行走动画
-    cocos2d::Animation* idleAnimations[6]; // 六个方向的待机动画
-    cocos2d::Vec2 targetPosition; // 目标位置
-    bool isMoving; // 是否正在移动
-
-    // 键盘状态
-    bool isWPressed, isAPressed, isSPressed, isDPressed;
-
-    // 最后一次行走的方向索引
-    int lastDirectionIndex;
-
-    // 创建六个方向的行走动画
-    void createWalkAnimations();
-
-    // 创建六个方向的待机动画
-    void createIdleAnimations();
-
-    // 根据方向选择动画
-    int getDirectionIndex(const cocos2d::Vec2& from, const cocos2d::Vec2& to);
-
-    // 移动主角到目标位置
-    void movePlayerTo(const cocos2d::Vec2& position);
-
-    // 更新主角位置
-    void updatePlayerPosition(float dt);
-
     // 每帧更新
-    void update(float dt);
+    void MainMap::update(float dt);
+
+private:
+    cocos2d::Sprite* mapSprite;    
+    Player player;
 };
 
 #endif // __MainMap_H__
